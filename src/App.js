641dab50a00823000998cf887c3edf336f5502a8
ParvejManer <parvej.maner@tekditechnologies.com>
2024-10-01 11:59:53 +0530

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { Box, Container, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import StudentForm from "./components/StudentForm";
 import BookForm from "./components/BookForm";
 import BookAllcateForm from "./components/BookAllcateForm";
+import StudentList from "./components/StudentList";
 
 const App = () => {
   return (
     <Router>
+      <AppBar component="nav" position="static" sx={{paddingBottom: '20px', marginBottom: '20px'}}>
+        <Toolbar>
+          <Typography variant="h5" color="inherit" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            Student Library Management System
+          </Typography>
+          <Button component={Link} color="inherit" to="/">Home</Button>
+          <Button component={Link} color="inherit" to="/addStudent">Add Student</Button>
+          <Button component={Link} color="inherit" to="/addBook">Add Book</Button>
+          <Button component={Link} color="inherit" to="/allocateBook">Book Allocate</Button>
+          <Button component={Link} color="inherit" to="/stuedntlist">Student List</Button>
+        </Toolbar>
+      </AppBar>
       <Container>
         <Routes>
           <Route
@@ -28,6 +41,7 @@ const App = () => {
           <Route path="/addStudent" element={<StudentForm />} />
           <Route path="/addBook" element={<BookForm />} />
           <Route path="/allocateBook" element={<BookAllcateForm />} />
+          <Route path="/stuedntlist" element={<StudentList />} />
         </Routes>
       </Container>
     </Router>
